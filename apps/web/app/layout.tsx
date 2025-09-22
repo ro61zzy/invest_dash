7f471e6087a622008c9de09efdc696b981604f8a
rozzy <rosekaremeri@gmail.com>
2025-09-22 16:07:54 +0300

    Fix: update metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,12 @@
  import './globals.css';
 import { ReactNode } from 'react';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Investment Dashboard",
+  description: "Track investments, monitor trends, and visualize your portfolio in real time.",
+};
+
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
